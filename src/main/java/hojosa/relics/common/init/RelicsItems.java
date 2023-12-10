306521f38c7165490a85fdc2ec9ca5ffbe64f5fa
Fox <mccloud.fox21@gmail.com>
2023-12-10 16:38:18 +0100

    added master sword

--- src/main/java/hojosa/relics/common/init/RelicsItems.java
+++ src/main/java/hojosa/relics/common/init/RelicsItems.java
@@ -1,7 +1,8 @@
 package hojosa.relics.common.init;
 
-import hojosa.relics.common.item.FirePlate;
+import hojosa.relics.common.item.FireTablet;
 import hojosa.relics.common.item.RelicsItem;
+import hojosa.relics.common.item.WaterTablet;
 import hojosa.relics.lib.References;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
@@ -12,14 +13,15 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class RelicsItems{
+	
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, References.MODID);
 	
-    public static final RegistryObject<FirePlate> FIRE_PLATE = ITEMS.register(
-            References.UnlocalizedName.FIRE_PLATE, () -> new FirePlate(1, Rarity.EPIC)
+    public static final RegistryObject<FireTablet> FIRE_TABLET = ITEMS.register(
+            References.UnlocalizedName.FIRE_PLATE, () -> new FireTablet(1, Rarity.EPIC)
     );
     
-    public static final RegistryObject<RelicsItem> WATER_PLATE = ITEMS.register(
-    		References.UnlocalizedName.WATER_PLATE, () -> new RelicsItem(1, Rarity.EPIC)
+    public static final RegistryObject<WaterTablet> WATER_TABLET = ITEMS.register(
+            References.UnlocalizedName.WATER_PLATE, () -> new WaterTablet(1, Rarity.EPIC)
     );
     
     public static final RegistryObject<RelicsItem> CLAY_BLUE = ITEMS.register(
@@ -34,11 +36,7 @@ public class RelicsItems{
             References.UnlocalizedName.FIRE_SWORD, () -> new SwordItem(Tiers.IRON, 3, -2.4F, (new Item.Properties().tab(References.ITEM_GROUP)))
     );
     
-    
-    
-    
-    
-    
-    
-    
+    public static final RegistryObject<SwordItem> MASTER_SWORD = ITEMS.register(
+            References.UnlocalizedName.MASTER_SWORD, () -> new SwordItem(Tiers.DIAMOND, 5, -2.4F, (new Item.Properties().tab(References.ITEM_GROUP)))
+    );
 }
