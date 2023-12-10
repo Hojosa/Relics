4f9c2bfab4e0560b3395a19bc7cd7a0b8260b52a
Fox <mccloud.fox21@gmail.com>
2023-12-10 16:45:00 +0100

    add curios tag datagen

--- src/main/java/hojosa/relics/common/datagen/providers/RelicsItemTags.java
+++ src/main/java/hojosa/relics/common/datagen/providers/RelicsItemTags.java
@@ -3,23 +3,15 @@ package hojosa.relics.common.datagen.providers;
 
 import org.jetbrains.annotations.Nullable;
 
-import hojosa.relics.common.init.RelicsItems;
+import hojosa.relics.Relics;
+import hojosa.relics.integration.curios.RelicsCuriosIntegration;
 import hojosa.relics.lib.References;
-import net.minecraft.core.Registry;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.BlockTagsProvider;
 import net.minecraft.data.tags.ItemTagsProvider;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.TagKey;
-import net.minecraft.world.item.Item;
-import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.data.ExistingFileHelper;
-import top.theillusivec4.curios.api.CuriosApi;
-import top.theillusivec4.curios.api.SlotTypePreset;
 
 public class RelicsItemTags extends ItemTagsProvider {
-	
-    public static final TagKey<Item> CHARM_TAG = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation(CuriosApi.MODID, SlotTypePreset.CHARM.getIdentifier()));
 
 	public RelicsItemTags(DataGenerator pGenerator, BlockTagsProvider pBlockTagsProvider, @Nullable ExistingFileHelper existingFileHelper) {
 		super(pGenerator, pBlockTagsProvider, References.MODID, existingFileHelper);
@@ -27,9 +19,9 @@ public class RelicsItemTags extends ItemTagsProvider {
 
 	@Override 
 	protected void addTags() {
-		tag(CHARM_TAG)
-			.add(RelicsItems.FIRE_PLATE.get());
-
+		if(Relics.curiosPresent){
+			RelicsCuriosIntegration.generateItemTags(this::tag);
+		}
 	}
 
     @Override
