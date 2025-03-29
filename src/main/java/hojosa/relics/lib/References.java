8b25890d4871487a709181265c1e1e526cda373d
Fox <mccloud.fox21@gmail.com>
2025-03-29 17:28:16 +0100

    not sure what i changed

--- src/main/java/hojosa/relics/lib/References.java
+++ src/main/java/hojosa/relics/lib/References.java
@@ -8,7 +8,7 @@ public class References {
 
 	public static final String MOD_ID = "relics";
 	public static final String VERSION = "0.3";
-	public static final String CREATIVE_TAB = "relics";
+	public static final String CREATIVE_TAB = "tab";
 
 	@NoArgsConstructor(access = AccessLevel.PRIVATE)
 	public static class UnlocalizedName {
