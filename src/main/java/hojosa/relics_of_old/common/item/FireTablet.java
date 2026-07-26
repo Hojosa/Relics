4ad6846daa19f83fc85f8e943cbf7766b79a57a9
Hojosa <mccloud.fox21@gmail.com>
2026-07-26 22:19:50 +0200

    remove unneeded inventoryTick from fireTablet

--- src/main/java/hojosa/relics_of_old/common/item/FireTablet.java
+++ src/main/java/hojosa/relics_of_old/common/item/FireTablet.java
@@ -4,14 +4,10 @@ import hojosa.relics_of_old.lib.item.RelicsItem;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
-import net.minecraft.world.level.Level;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import top.theillusivec4.curios.api.CuriosApi;
-import top.theillusivec4.curios.api.CuriosCapability;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.type.capability.ICurio;
 
@@ -41,13 +37,5 @@ public class FireTablet extends RelicsItem
 	    		return true;
 	    	}
 	    });
-	}
-	
-	@Override
-	public void inventoryTick(ItemStack pStack, Level pLevel, Entity pEntity, int pSlotId, boolean pIsSelected) {
-		if(!pStack.getCapability(CuriosCapability.ITEM).isPresent()) {
-			Player player = (Player)pEntity;
-			player.addEffect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 20));
-		}
 	}
 }
