05fcf349af5332502e7b793b6455719df1c31c67
Hojosa <mccloud.fox21@gmail.com>
2026-08-07 16:00:11 +0200

    clean up particle reg. class

--- src/main/java/hojosa/relics_of_old/common/init/RelicsParticles.java
+++ src/main/java/hojosa/relics_of_old/common/init/RelicsParticles.java
@@ -18,12 +18,7 @@ public class RelicsParticles {
 	public static final DeferredRegister<ParticleType<?>> PARTICLE_TYPES = DeferredRegister.create(ForgeRegistries.PARTICLE_TYPES, References.MOD_ID);
 
 	public static final RegistryObject<SimpleParticleType> FLAME_PATTICLES = PARTICLE_TYPES.register(References.UnlocalizedName.FLAME_PARTICLE, () -> new SimpleParticleType(true));
-//	public static final RegistryObject<SimpleParticleType> STAR_BEAM_TORCH_PATTICLES = PARTICLE_TYPES.register(References.UnlocalizedName.STAR_BEAM_TORCH_PARTICLES, () -> new SimpleParticleType(true));
-
-//	public static final RegistryObject<SimpleParticleType> STAR_BEAM_GRIND_PATTICLES = PARTICLE_TYPES.register(References.UnlocalizedName.STAR_BEAM_GRIND_PARTICLES, () -> new SimpleParticleType(true));
-
 	public static final RegistryObject<SimpleParticleType> SPARKLE_PARTICLES = PARTICLE_TYPES.register(References.UnlocalizedName.SPARKLE_PATTICLES, () -> new SimpleParticleType(true));
-
 	public static final RegistryObject<ParticleType<RelicsParticleOptions>> RUNE_PARTICLE = PARTICLE_TYPES.register(References.UnlocalizedName.RUNE_PARTICLE,
 			() -> new ParticleType<>(true, RelicsParticleOptions.DESERIALIZER) {
 				@Override
@@ -33,12 +28,11 @@ public class RelicsParticles {
 			});
 
 	public static final RegistryObject<SimpleParticleType> FLARE_PARTICLE = PARTICLE_TYPES.register(References.UnlocalizedName.FLARE_PARTICLE, () -> new SimpleParticleType(true));
-
 	public static final RegistryObject<ParticleType<RelicsParticleOptions>> MAGIC_SCRAMBLE_PARTICLE = PARTICLE_TYPES.register(References.UnlocalizedName.MAGIC_SCRAMBLE_PARTICLE,
-	          () -> new ParticleType<>(true, RelicsParticleOptions.DESERIALIZER) {
-	              @Override
-	              public Codec<RelicsParticleOptions> codec() {
-	                  return RelicsParticleOptions.codec(MAGIC_SCRAMBLE_PARTICLE);
-	              }
-	          });
+			() -> new ParticleType<>(true, RelicsParticleOptions.DESERIALIZER) {
+				@Override
+				public Codec<RelicsParticleOptions> codec() {
+					return RelicsParticleOptions.codec(MAGIC_SCRAMBLE_PARTICLE);
+				}
+			});
 }
