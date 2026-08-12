a88b297793ba893716782fd0cef53c50bd3ae85c
Hojosa <mccloud.fox21@gmail.com>
2026-08-12 22:33:31 +0200

    remove unused import

--- src/main/java/hojosa/relics_of_old/common/block/SugarCubeBlock.java
+++ src/main/java/hojosa/relics_of_old/common/block/SugarCubeBlock.java
@@ -16,7 +16,6 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.FallingBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
